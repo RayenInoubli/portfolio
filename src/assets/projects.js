8a8b7bdc406fe87440dba74bafac79173dc0e625
Rayen_In <rayeninoubli29@gmail.com>
2024-06-09 19:34:35 +0100

    fix images import

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -3,21 +3,21 @@ export const projects = [
     id: 1,
     title: "Dentixio",
     description: "dental firm management system",
-    image: "../src/assets/dentixio.jpg",
+    image: "../src/assets/images/dentixio.jpg",
     url: "https://dentixio.netlify.app/"
   },
   {
     id: 3,
     title: "Magano CRM",
     description: "Call center management system",
-    image: "../src/assets/magano.jpg",
+    image: "../src/assets/images/magano.jpg",
     url: "#"
   },
   {
     id: 4,
     title: "Creadevity website",
     description: "A website for Creadevity, a rising startup",
-    image: "../src/assets/creadevity.jpg",
+    image: "../src/assets/images/creadevity.jpg",
     url: "https://creadevity.tn/"
   },
 ];
